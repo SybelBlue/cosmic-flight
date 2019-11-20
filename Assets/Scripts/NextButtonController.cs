e106970edcda157c29ff34d34cf4ccb4329a9d54
jxu913 <jxu@macalester.edu>
2019-11-19 19:35:52 -0600

    started tutorial page

---
+++ Assets/Scripts/NextButtonController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextButtonController : MonoBehaviour
+{
+	public Sprite[] imageArray;
+	public SpriteRenderer fieldRenderer;
+	public int currentImage = 0;
+
+    public void OnClick(int gotoImage) // +1 for Next, -1 for Previous
+    {
+		if (currentImage < imageArray.Length) {
+			currentImage += gotoImage;
+			fieldRenderer.sprite = imageArray[currentImage];
+		}
+	}
+}
