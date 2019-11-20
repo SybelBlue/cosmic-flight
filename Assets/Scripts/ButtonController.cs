3c7d36eb3e147ca85bd46927c599e249cf192294
jxu913 <jxu@macalester.edu>
2019-11-20 12:13:35 -0600

    Added instructions to tutorial scene

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
 	public Sprite[] imageArray;
-	public int currentImage = 0;
+	public int page = 0;
     public SpriteRenderer fieldRenderer;
+	public string[] instructions;
+    public Text displayer; 
 
     public void Start()
     {
-        fieldRenderer.sprite = imageArray[currentImage];
+        fieldRenderer.sprite = imageArray[page];
+		displayer.text = instructions[page];
     }
 
-    public void OnClick(int gotoImage)
+    public void OnClick(int gotoPage)
     {
-        currentImage += gotoImage;
-        fieldRenderer.sprite = imageArray[currentImage];
+        page += gotoPage;
+		if (page < 0 || page >= imageArray.Length){
+			SceneManager.LoadScene("Title");
+		}
+        fieldRenderer.sprite = imageArray[page];
+		displayer.text = instructions[page];
     }
 }
