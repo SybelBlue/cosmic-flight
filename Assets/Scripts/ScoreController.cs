4e7d36a533c546dbb1ef1dcf60addd5d3aa99fed
jxu913 <jxu@macalester.edu>
2019-11-24 16:51:47 -0600

    changed scoring

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -4,12 +4,30 @@ using UnityEngine.UI;
 public class ScoreController : MonoBehaviour
 {
 	public string scoreStr;
+	public int score;
 	public Text displayer;
 	
 
-    internal void SetScore(int launchCounts)
+    internal void SetScore(int asteroidCounts, int launchCounts)
     {
-        scoreStr = "Your Score: " + (500 - (launchCounts-1) * 5);
+		if (asteroidCounts == 0) {
+			score = 500 - (launchCounts-1) * 5;
+		}
+        else if (asteroidCounts == 1) {
+			score = 500 - (launchCounts-1) * 5 + asteroidCounts * 10;
+		}
+		else if (asteroidCounts == 2) {
+			score = 500 - (launchCounts-1) * 6 + asteroidCounts * 10;
+		}
+		else {
+			score = 500 - (launchCounts-1) * 8 + asteroidCounts * 10;
+		}
+		if (score >= 485)
+			scoreStr = "Your Score: 3/3";
+		else if (score >= 470 && score < 485)
+			scoreStr = "Your Score: 2/3";
+		else
+			scoreStr = "Your Score: 1/3";
 		displayer.text = scoreStr;
     }
 }
