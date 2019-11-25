573ab33b527d3315d48c698bcf437584bfa19388
SybelBlue <lbc.dna@gmail.com>
2019-11-25 09:02:09 -0600

    Fixed overlapping music bug

--- Assets/Scripts/PersistBetweenScenes.cs
+++ Assets/Scripts/PersistBetweenScenes.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PersistBetweenScenes : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public bool stayUnique = true;
+
+    private static List<string> uniqueObjects = new List<string>();
+
     void Start()
     {
-        // GameObject.FindGameObjectsWithTag("Music and Effects")
+        if (stayUnique && uniqueObjects.Contains(gameObject.name))
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(gameObject);   
+
+        if (stayUnique)
+        {
+            uniqueObjects.Add(gameObject.name);
+        }
     }
 }
