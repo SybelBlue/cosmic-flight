9b75b0994c954fc156b6575966407506c580839f
SybelBlue <lbc.dna@gmail.com>
2019-11-25 08:55:25 -0600

    Refactored using Scene Loader

--- Assets/Scripts/PersistBetweenScenes.cs
+++ Assets/Scripts/PersistBetweenScenes.cs
@@ -5,6 +5,7 @@ public class PersistBetweenScenes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // GameObject.FindGameObjectsWithTag("Music and Effects")
         DontDestroyOnLoad(gameObject);   
     }
 }
