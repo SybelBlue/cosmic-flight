59a25d9ca8338bc510e7787e6b007027afc30c75
jxu913 <jxu@macalester.edu>
2019-12-12 10:52:44 -0600

    update script

--- Assets/Scripts/Controllers/TutorialPageController.cs
+++ Assets/Scripts/Controllers/TutorialPageController.cs
@@ -4,18 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class TutorialPageController : MonoBehaviour
 {
-	//public Sprite[] imageArray;
 	public GameObject[] tutorialArray;
 	public int page = 0;
-    //public SpriteRenderer fieldRenderer;
-	//public string[] instructions;
-    //public Text displayer; 
-
+    
     public void Start()
     {
-        //fieldRenderer.sprite = imageArray[page];
 		tutorialArray[page].SetActive(true);
-		//displayer.text = instructions[page];
     }
 
     public void ChangePageNumberBy(int gotoPage)
@@ -26,11 +20,9 @@ public class TutorialPageController : MonoBehaviour
 			SceneManager.LoadScene("Title");
 			return;
 		}
-        //fieldRenderer.sprite = imageArray[page];
-		//tutorialArray[page].SetActive(true);
+
 		for (int i = 0; i < tutorialArray.Length; i++) {
 			tutorialArray[i].SetActive(i == page);
 		}
-		//displayer.text = instructions[page];
     }
 }
